6753377c9d2653ce0471c715106a7846d56d9e1c
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2018-07-07 12:27:04 -0700

    get batch meta(available jitter startus)  for proper enqueueing without many dead jobs

--- oddball_db.py
+++ oddball_db.py
@@ -1,9 +1,13 @@
+import pandas as pd
 import nems.modelspec as ms
+import nems_db.db as nd
+import nems_db.baphy as nb
 import os
 import joblib as jl
 
 # important db root path
-root_path = '/auto/users/mateo/oddball_results'
+this_script_dir = os.path.dirname(os.path.realpath(__file__))
+pickles_path = '{}/pickles'.format(this_script_dir)
 
 # less important test cache path
 cache_path = '/home/mateo/oddball_analysis/pickles'
@@ -91,3 +95,80 @@ def load_from_cache(cellid, batch, modelname):
     ctx = jl.load(filepath)
 
     return ctx
+
+def get_single_metadata(cellid, batch):
+    '''
+    returns cell file metadata, like experiment parameters, right now only gets jitter values
+    :param cellid:
+    :param batch:
+    :return:
+    '''
+
+    # check what files are asociated to this cellid batch combination
+
+    parmfiles = nd.get_batch_cell_data(cellid=cellid, batch=296)
+    parmfiles = list(parmfiles['parm'])
+    # creates a dictionary mapping the epoch keys to the parmfiles paths, i.e.
+    # from: 'FILE_gus037d03_p_SSA' to: '/auto/data/daq/Augustus/gus037/gus037d03_p_SSA.m'
+    parmfiles = ['{}.m'.format(path) for path in parmfiles]
+
+
+    # ToDo add here whatever metatdata is important to extract and tabulate
+    cell_meta = dict.fromkeys(['cellid', 'jitter_status'])
+    cell_meta['cellid'] = cellid
+
+    # to relate filename to jitter status pulls experimetn parameters
+    jitter_status = list()
+    for pfile in parmfiles:
+        globalparams, exptparams, exptevents = nb.baphy_parm_read(pfile)
+
+        # convoluted indexig into the nested dictionaries ot get Jitter status, sets value to "Off" by default
+        j_stat = exptparams['TrialObject'][1]['ReferenceHandle'][1].get('Jitter', 'Off')
+        jitter_status.append('Jitter_{}'.format(j_stat.rstrip()))
+
+    cell_meta['jitter_status'] = jitter_status
+
+
+    df = pd.DataFrame([cell_meta])
+
+def get_batch_metadata(batch, recache=False):
+    '''
+    returns a DF wit metadata of all the cells in a batch. this metadata is relevanta at the moment of decidign what
+    modelse to fit to the cells.
+    In the case of
+
+    :param batch:
+    :return:
+    '''
+
+    filename = 'batch_meta'
+    meta_db_path = os.path.normcase('{}/{}'.
+                                    format(pickles_path, filename))
+
+    if os.path.exists(meta_db_path) and recache == False:
+        print('df loaded from cache ')
+        DF = jl.load(meta_db_path)
+        return DF
+
+    else:
+        print('extracting meta de novo')
+
+    batch_cells = nd.get_batch_cells(batch=296).cellid
+
+    metas = list()
+
+    for cellid in batch_cells:
+        df = get_single_metadata(cellid, batch)
+        metas.append(df)
+
+    DF = pd.concat(metas)
+
+    jl.dump(DF, meta_db_path)
+
+    return DF
+
+
+
+
+
+
